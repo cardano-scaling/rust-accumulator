97901592d75582733bf62380c2318b07d3f18711
Daniel Firth <daniel.firth@iohk.io>
2026-01-21 13:30:32 +0000

    Reenable musl for x86_64-linux

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -1,10 +1,12 @@
 { self, inputs, ...}: {
   perSystem = {system, pkgs, lib, ...}:
+
     let
       toolchain = with inputs.fenix.packages.${system};
         combine [
           minimal.rustc
           minimal.cargo
+          targets.x86_64-unknown-linux-musl.latest.rust-std
         ];
 
       naersk' = inputs.naersk.lib.${system}.override {
@@ -21,6 +23,9 @@
         stdenv = pkgs.stdenv;
         doCheck = false;
         copyLibs = true;
+
+        CARGO_BUILD_TARGET = if system == "x86_64-linux" then "x86_64-unknown-linux-musl" else null;
+        CARGO_BUILD_RUSTFLAGS = if system == "x86_64-linux" then "-C target-feature=+crt-static" else null;
 
         postInstall = ''
           mkdir -p $out/lib/pkgconfig

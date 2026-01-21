4e17024754a71bbcb9ea5224d3203f82373f6e79
Daniel Firth <daniel.firth@iohk.io>
2026-01-21 11:45:11 +0000

    Move include postInstall to real directory

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -33,31 +33,7 @@
 
           version=$(grep '^version = "[^"]*"' Cargo.toml | cut -d '"' -f2 | head -n1)
 
-          mkdir -p $out/include
-          cat > $out/include/rust_accumulator.h <<'EOF'
-          #include "blst.h"
-          #include <stddef.h>
-
-          // Define the Scalar structure as it is in Rust
-          typedef struct {
-            blst_fr inner;
-          } Scalar;
-
-          // Define the G1Projective structure as it is in Rust
-          typedef struct {
-            blst_p1 inner;
-          } G1Projective;
-
-          // Define the G2Projective structure as it is in Rust
-          typedef struct {
-            blst_p2 inner;
-          } G2Projective;
-
-          void get_poly_commitment_g1(G1Projective *return_point, Scalar *scalars_ptr, size_t scalars_len, G1Projective *points_ptr, size_t points_len);
-
-          void get_poly_commitment_g2(G2Projective *return_point, Scalar *scalars_ptr, size_t scalars_len, G2Projective *points_ptr, size_t points_len);
-
-          EOF
+          cp "${self}/include" $out/ -r
 
           cat > $out/lib/pkgconfig/librust_accumulator.pc <<EOF
           prefix=$out
